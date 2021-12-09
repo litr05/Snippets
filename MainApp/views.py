6d28310bd899800059aae35090502f39a02c2f37
Litr05 <litrovkin@yandex.ru>
2021-12-09 15:18:12 +0300

    complete task-1 module-6

--- MainApp/views.py
+++ MainApp/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, redirect
 from MainApp.models import Snippet
 from django.core.exceptions import ObjectDoesNotExist
 from MainApp.forms import SnippetForm
-
+from django.contrib import auth
 
 def index_page(request):
     context = {'pagename': 'PythonBin'}
@@ -17,6 +17,13 @@ def snippets_page(request):
                }
     return render(request, 'pages/view_snippets.html', context)
 
+def snippets_my(request):
+    user_snippets = Snippet.objects.filter(user=request.user)
+    context = {
+        'pagename': 'Мои сниппеты',
+        "snippets": user_snippets
+    }
+    return render(request, 'pages/view_snippets.html', context)
 
 def single_snippet_page(request, id):
     try:
@@ -61,7 +68,10 @@ def add_snippet_page(request):
     if request.method == "POST":
         form = SnippetForm(request.POST)
         if form.is_valid():
-            form.save()
+            snippet = form.save(commit=False)
+            if request.user.is_authenticated:
+                snippet.user = request.user
+                snippet.save()
             return redirect("snippets-list")
         return render(request, 'add_snippet.html', {'form': form})
 
@@ -100,8 +110,11 @@ def login_page(request):
         if user is not None:
             auth.login(request, user)
         else:
-            # Return error message
-            pass
+            context = {
+                'pagename': "PythonBin",
+                'errors': ['не верный логин или пароль']
+            }
+            return render(request, 'pages/index.html', context)
 
     return redirect('home')
 
--- Snippets/urls.py
+++ Snippets/urls.py
@@ -1,22 +1,6 @@
-"""Snippets URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-
 from MainApp import views
 
 
@@ -25,7 +9,7 @@ urlpatterns = [
     path('snippets/add', views.add_snippet_page, name="snippets-add"),
     path('snippet/page/<int:id>', views.single_snippet_page, name="snippet-page"),
     path('snippets/list', views.snippets_page, name="snippets-list"),
-    # path('create_new_snippet', views.create_new_snippet),
+    path('snippets/my', views.snippets_my, name="snippets-my"),
     path('snippet/delete/<int:id>', views.snippet_delete, name="snippet-delete"),
     path('snippet/edit/<int:id>', views.snippet_edit, name="snippet-edit"),
     path('auth/login', views.login_page, name="login"),
